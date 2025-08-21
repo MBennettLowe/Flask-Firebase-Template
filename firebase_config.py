417c75cf020217717b1e5b74acba9d37432ec2c4
MBennettLowe <mbennettlowe@gmail.com>
2025-08-21 18:08:10 -0400

    Added .gitignore and config files

--- firebase_config.py
+++ firebase_config.py
@@ -1,4 +1,10 @@
 # This file contains the firebase configuration details.
 firebaseConfig = {
-
-}
+  "apiKey": "AIzaSyCNzlPTJMbqiKEEArqyEOVeD_Se6QMcOuA",
+  "authDomain": "quadratic-poll.firebaseapp.com",
+  "projectId": "quadratic-poll",
+  "storageBucket": "quadratic-poll.firebasestorage.app",
+  "messagingSenderId": "864268235842",
+  "appId": "1:864268235842:web:146ed75d6bc810dcc769db",
+  "measurementId": "G-W3KTQ563WZ"
+};
--- static/firebase-config.js
+++ static/firebase-config.js
@@ -5,8 +5,15 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  "apiKey": "AIzaSyCNzlPTJMbqiKEEArqyEOVeD_Se6QMcOuA",
+  "authDomain": "quadratic-poll.firebaseapp.com",
+  "projectId": "quadratic-poll",
+  "storageBucket": "quadratic-poll.firebasestorage.app",
+  "messagingSenderId": "864268235842",
+  "appId": "1:864268235842:web:146ed75d6bc810dcc769db",
+  "measurementId": "G-W3KTQ563WZ"
+}
 
-};
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
